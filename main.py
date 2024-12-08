72bafd1b6648c042e5fb6d15676449aafb33ca03
Esorte <76818934+Esorte@users.noreply.github.com>
2024-12-08 20:31:54 +0700

    update to dict

--- main.py
+++ main.py
@@ -13,22 +13,32 @@ API_KEY = 'mai mee'
 URL = "https://maps.googleapis.com/maps/api/distancematrix/json"
 
 # Locations
-origin1 = "long, lat"  # bkk
-destination1 = "long, lat"  # muic
+
+location_map = {
+    (???, ???): (???, ???),  # ori -> dest
+    (???, ???): (???, ???)
+
+}
+# origin1 = "long, lat"  # bkk
+# destination1 = "long, lat"  # muic
 
 while True:
-    response = requests.get(URL, params={
-        "origin1": origins,
-        "destination1": destinations,
-        "departure_time": "now",
-        "key": API_KEY
-    })
-    
-    if response.status_code == 200:
-        data = response.json()
-        producer.send('traffic_data', value=data)
-        print("Data sent to Kafka:", data)
-    else:
-        print("Failed to get data:", response.status_code, response.text)
-    
+    for (origin_lat, origin_long), (dest_lat, dest_long) in location_map.items():
+        origins = f"{origin_lat},{origin_long}"
+        destinations = f"{dest_lat},{dest_long}"
+        
+        response = requests.get(URL, params={
+            "origins": origins,
+            "destinations": destinations,
+            "departure_time": "now",
+            "key": API_KEY
+        })
+        
+        if response.status_code == 200:
+            data = response.json()
+            producer.send('traffic_data', value=data)
+            print("Data sent to Kafka:", data)
+        else:
+            print("Failed to get data:", response.status_code, response.text)
+        
     time.sleep(60)  # run every minute
